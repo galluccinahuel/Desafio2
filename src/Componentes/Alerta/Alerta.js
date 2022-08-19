33f1250bae5f60e5a00a031ab689d5f319c5f479
galluccinahuel <galluccinahuel@gmail.com>
2022-08-19 00:21:27 -0300

    3

---
+++ src/Componentes/Alerta/Alerta.js
@@ -0,0 +1,17 @@
+import './Alerta.css';
+
+function Alerta(){
+    return(
+        <div className="alerta-contenedor">
+            <p className='alerta-parrafo'>
+                Parrafo Alerta
+            </p>
+            <h1>
+                h1
+            </h1>
+        </div>
+    )
+}
+
+
+export default Alerta;
